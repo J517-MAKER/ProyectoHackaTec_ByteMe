768651d276a8f810288e4a7ea62fecbc1fa20967
Pegaso <azulijuan6@gmail.com>
2026-05-12 13:27:24 -0600

    Nuevo Inicio

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginScreen } from "./components/LoginScreen";
-
-export default function App() {
-  return (
-    <div className="size-full bg-background">
-      <LoginScreen />
-    </div>
-  );
-}
